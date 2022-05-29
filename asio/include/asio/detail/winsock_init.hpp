6ec064b0d96f3442664664d5e32558392f862c08
Daniela Engert <dani@ngrt.de>
2022-05-29 07:18:20 +0200

    Don't export definitions with internal linkage (e.g. 'static')

--- asio/include/asio/detail/winsock_init.hpp
+++ asio/include/asio/detail/winsock_init.hpp
@@ -112,7 +112,9 @@ winsock_init_base::data winsock_init<Major, Minor>::data_;
 
 // Static variable to ensure that winsock is initialised before main, and
 // therefore before any other threads can get started.
+#ifndef ASIO_MODULE
 static const winsock_init<>& winsock_init_instance = winsock_init<>(false);
+#endif
 
 } // namespace detail
 } // namespace asio
